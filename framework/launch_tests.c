98f2b2e541493b3b63b194fdc107501a48149b8d
Yacoub Altayeh (Home) <yacoub.altayeh@icloud.com>
2025-07-17 20:52:34 +0300

    Implement launch_tests function to iterate and print test details

--- framework/launch_tests.c
+++ framework/launch_tests.c
@@ -2,5 +2,17 @@
 
 int	launch_tests(t_unit **tests_list)
 {
-	(void)tests_list;
+	t_unit	*cur;
+	int		i;
+
+	cur = *tests_list;
+	while (cur && cur->next) // stop in last node
+		cur = cur->next;
+	i = 0;
+	while (cur)
+	{
+		printf("%i %s %p\n", i, cur->test_name, cur->test_func);
+		cur = cur->prev;
+		i++;
+	}
 }
